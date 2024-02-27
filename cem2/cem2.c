7856b64fb69945503f1643f4537c71fa332d50a9
daohoangtuyenkc <56294182+daohoangtuyenkc@users.noreply.github.com>
2024-02-27 14:57:59 +0300

    Update cem2.c

--- cem2/cem2.c
+++ cem2/cem2.c
@@ -1,1 +1,6 @@
-c
+chạy make
+sau đó sudo insmod main.ko để thêm cái file main.ko này vào log kernel
+sudo dmesg để xem log, nó sẽ in ra hello world
+muốn xóa thì sudo rmmod main 
+sudo dmesg nó sẽ in ra goodbye world
+
